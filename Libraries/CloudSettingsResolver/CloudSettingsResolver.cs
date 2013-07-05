3a8527a4761632aa9942558673b56f3e625e3cb9
Alex Fournier <zander@live.fr>
2013-07-05 11:35:54 +0200

    Updated projects settings

---
+++ Libraries/CloudSettingsResolver/CloudSettingsResolver.cs
@@ -0,0 +1,33 @@
+﻿using System.Configuration;
+using Microsoft.WindowsAzure.ServiceRuntime;
+
+namespace website_mvc.Code
+{
+    public static class CloudSettingsResolver
+    {
+        private static bool inCloud = false;
+
+        static CloudSettingsResolver()
+        {
+            inCloud = RoleEnvironment.IsAvailable;
+        }
+
+        public static string GetConnectionString(string name)
+        {
+            string connectionString = (inCloud)
+                        ? RoleEnvironment.GetConfigurationSettingValue(name)
+                        : ConfigurationManager.ConnectionStrings[name].ConnectionString;
+
+            return connectionString;
+        }
+
+        public static string GetConfigSetting(string name)
+        {
+            string setting = (inCloud)
+                        ? RoleEnvironment.GetConfigurationSettingValue(name)
+                        : ConfigurationManager.AppSettings[name];
+
+            return setting;
+        }
+    }
+}
